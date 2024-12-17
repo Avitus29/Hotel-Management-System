66e06866d455b39f63517b2b5da14e8cc2eee595
Avitus Valence <avitusvalence6@gmail.com>
2024-12-17 08:53:00 +0300

    Project Initialization

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include <string>
+#include <fstream>
+#include <vector>
+
+using namespace std;
+
+struct Hotel {
+    int RoomID;
+    string CustomerName,BookingDate,price;
+};
